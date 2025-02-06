e8a39d31573fd8a0f8e0ab354eb6cac114872c0a
Ryan Prichard <rprichard@google.com>
2025-02-06 13:14:34 -0800

    Revert^2 "Upgrade to clang-r547379"
    This reverts commit edd39aee55e758219a1c91f4db4399d796ddb5f8.
    
    Reason for revert: reland compiler update
    
    Change-Id: I51bd7bd16df5a41afb847587f952c9756070627c

--- cc/config/global.go
+++ cc/config/global.go
@@ -390,8 +390,8 @@ var (
 
 	// prebuilts/clang default settings.
 	ClangDefaultBase         = "prebuilts/clang/host"
-	ClangDefaultVersion      = "clang-r536225"
-	ClangDefaultShortVersion = "19"
+	ClangDefaultVersion      = "clang-r547379"
+	ClangDefaultShortVersion = "20"
 
 	// Directories with warnings from Android.bp files.
 	WarningAllowedProjects = []string{
